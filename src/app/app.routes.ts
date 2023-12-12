3e9b1df2febe52d9ce96c1aac6f35d001cb4e3da
abdelghafour <miso.abdo@gmail.com>
2023-12-12 14:33:32 +0100

    working on competitons & dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CreateCompetitionComponent } from './components/create-competition/create-competition.component';
+import { CompetitionsComponent } from './components/competitions/competitions.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 export const routes: Routes = [
       { path: 'home', component: HomeComponent },
       { path: 'competition/create', component: CreateCompetitionComponent },
+      { path: 'competitions', component: CompetitionsComponent },
+      { path: 'registration', component: RegistrationComponent },
+      // { path: 'competition/:id', component: CreateCompetitionComponent },
       { path: '', redirectTo: '/home', pathMatch: 'full' },
       { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
