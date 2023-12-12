1d64f24df4876dfddc0164f270e7ea96858deba4
abdelghafour <miso.abdo@gmail.com>
2023-12-12 09:30:26 +0100

    working on create competition

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CreateCompetitionComponent } from './components/create-competition/create-competition.component';
 
 export const routes: Routes = [
       { path: 'home', component: HomeComponent },
+      { path: 'competition/create', component: CreateCompetitionComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,47 +3,13 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   prefix: '',
   mode: 'jit',
-  important: false,
+  important: true,
   content: ["./src/**/*.{html,ts}"],
   darkMode: 'class',
   theme: {
 
     extend: {
-      colors: {
-        gray: {
-          100: '#FBFBFB',
-          200: '#EAEAEA',
-          300: '#DFDFDF',
-          400: '#999999',
-          500: '#7F7F7F',
-          600: '#666666',
-          700: '#4C4C4C',
-          800: '#333333',
-          900: '#191919',
-        },
-        blue: {
-          100: '#E6F0FD',
-          200: '#CCE2FC',
-          300: '#99C5FA',
-          400: '#66A9F7',
-          500: '#338CF5',
-          600: '#0070F4',
-          700: '#0064DA',
-          800: '#0059C2',
-          900: '#004391',
-        },
-        teal: {
-          100: '#E6FFFA',
-          200: '#B2F5EA',
-          300: '#81E6D9',
-          400: '#4FD1C5',
-          500: '#3ABAB4',
-          600: '#319795',
-          700: '#2C7A7B',
-          800: '#285E61',
-          900: '#234E52',
-        },
-      },
+
       boxShadow: {
         xs: '0 0 0 1px rgba(0, 0, 0, 0.16)',
         sm: '0 1px 2px 0 rgba(0, 0, 0, 0.16)',
@@ -56,55 +22,12 @@ module.exports = {
         outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
         none: 'none',
       },
-      spacing: {
-        '9/16': '56.25%',
-        '3/4': '75%',
-        '1/1': '100%',
-      },
+
       fontFamily: {
         inter: ['var(--font-inter)', 'sans-serif'],
       },
-      fontSize: {
-        xs: '0.75rem',
-        sm: '0.875rem',
-        base: '1rem',
-        lg: '1.125rem',
-        xl: '1.25rem',
-        '2xl': '1.5rem',
-        '3xl': '2rem',
-        '4xl': '2.625rem',
-        '5xl': '3.25rem',
-        '6xl': '5.5rem',
-      },
-      inset: {
-        '1/2': '50%',
-        'full': '100%',
-      },
-      letterSpacing: {
-        tighter: '-0.02em',
-        tight: '-0.01em',
-        normal: '0',
-        wide: '0.01em',
-        wider: '0.02em',
-        widest: '0.4em',
-      },
-      lineHeight: {
-        none: '1',
-        tighter: '1.125',
-        tight: '1.25',
-        snug: '1.375',
-        normal: '1.5',
-        relaxed: '1.625',
-        loose: '2',
-        '3': '.75rem',
-        '4': '1rem',
-        '5': '1.2rem',
-        '6': '1.5rem',
-        '7': '1.75rem',
-        '8': '2rem',
-        '9': '2.25rem',
-        '10': '2.5rem',
-      },
+
+
       minWidth: {
         '10': '2.5rem',
         '48': '12rem',
