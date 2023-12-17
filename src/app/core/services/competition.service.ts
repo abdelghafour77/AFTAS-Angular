56cf3c2cd1aba214cccef9799c510f38d2ab348a
abdelghafour <miso.abdo@gmail.com>
2023-12-17 22:23:12 +0100

    add services

--- src/app/core/services/competition.service.ts
+++ src/app/core/services/competition.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { Competition } from '../models/competition';
+import { Member } from '../models/member';
+import { Ranking } from '../models/ranking';
 
 
 const httpOptions = {
@@ -22,10 +24,10 @@ export class CompetitionService {
     return this.http.get<Competition[]>(this.apiUrl);
   }
 
-  getCompetition(id: number): Observable<Competition> {
-    const url = `${this.apiUrl}/${id}`;
-    return this.http.get<Competition>(url);
-  }
+  // getCompetition(id: number): Observable<Competition> {
+  //   const url = `${this.apiUrl}/${id}`;
+  //   return this.http.get<Competition>(url);
+  // }
 
   createCompetition(competition: Competition): Observable<Competition> {
     return this.http.post<Competition>(this.apiUrl, competition, httpOptions);
@@ -36,9 +38,21 @@ export class CompetitionService {
     return this.http.get<Competition[]>(url);
   }
 
-  // filterCompetitions(filter: string): Observable<Competition[]> {
-  //   const url = `${this.apiUrl}/filter/${filter}`;
-  //   return this.http.get<Competition[]>(url);
-  // }
+  getCompetitionsByStatus(status: string): Observable<Competition[]> {
+    const url = `${this.apiUrl}/status/${status}`;
+    return this.http.get<Competition[]>(url);
+  }
+
+  getCompetitionsByCode(code: string): Observable<Ranking[]> {
+    const url = `${this.apiUrl}/code/${code}`;
+    return this.http.get<Member>(url).pipe(map(result => result.rankings));
+  }
+
+  registerMemberToCompetition(competitionCode: string, memberId: number): Observable<Competition> {
+    console.log("competitionCode: " + competitionCode);
+    const url = `${this.apiUrl}/register/${competitionCode}/${memberId}`;
+    return this.http.put<Competition>(url, httpOptions);
+
+  }
 
 }
