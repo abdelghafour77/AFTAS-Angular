2788bf8ef612ee56b34824d7eac967b3ed6f9c51
abdelghafour <miso.abdo@gmail.com>
2023-12-21 16:46:06 +0100

    add pagination

--- src/app/components/competitions/competitions.component.ts
+++ src/app/components/competitions/competitions.component.ts
@@ -6,6 +6,10 @@ import { CompetitionService } from '../../core/services/competition.service';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Member } from '../../core/models/member';
+import { RouterLink } from '@angular/router';
+import { CCompetitionPage } from '../../core/models/competitionPage';
+import { PaginationComponent } from '../pagination/pagination.component';
+import { Flowbite } from '../../core/config/flowbite-config';
 // reactive forms
 
 
@@ -13,45 +17,75 @@ import { Member } from '../../core/models/member';
   selector: 'app-competitions',
   standalone: true,
   imports: [
+    FormsModule,
+    PaginationComponent,
     ReactiveFormsModule,
+    RouterLink,
     CommonModule
   ],
   templateUrl: './competitions.component.html',
   styleUrl: './competitions.component.css'
 })
+@Flowbite()
 export class CompetitionsComponent implements OnInit {
 
-  competitions: Competition[] = [];
+  competitions: CCompetitionPage = new CCompetitionPage();
+
   searchControl = new FormControl();
   currentCompetition?: Competition;
+  loading = true;
+  page = 0;
+  size = 5;
+  search = '';
+
+
 
   constructor(private competitionService: CompetitionService) { }
   activeStatus = 'all';
+
   ngOnInit() {
-    this.competitionService.getCompetitions().subscribe(competitions => this.competitions = competitions);
+    // this.pages = Array(100).fill(0).map((x, i) => i + 1);
+
+    this.competitionService.getPaginateCompetitions(this.page).subscribe(competitions => this.competitions = competitions);
 
     this.searchControl.valueChanges.pipe(
-      debounceTime(200),
+      debounceTime(700),
       distinctUntilChanged()
     ).subscribe(searchTerm => {
       if (searchTerm.length <= 0)
-        this.competitionService.getCompetitions().subscribe(competitions => this.competitions = competitions);
+        this.competitionService.getPaginateCompetitions(this.page).subscribe(competitions => this.competitions = competitions);
       else
-        this.competitionService.searchByCriteria(searchTerm).subscribe(competitions => this.competitions = competitions);
+        this.competitionService.searchByCriteriaPaginate(searchTerm, this.page).subscribe(competitions => this.competitions = competitions);
     });
   }
+
   getCompetitionsByStatus(status: string) {
+    this.page = 0;
     this.activeStatus = status;
     if (status == 'all')
-      this.competitionService.getCompetitions().subscribe(competitions => this.competitions = competitions);
+      this.competitionService.getPaginateCompetitions(this.page).subscribe(competitions => this.competitions = competitions);
     else
-      this.competitionService.getCompetitionsByStatus(status)
+      this.competitionService.getCompetitionsByStatusPaginate(status, this.page)
         .subscribe(competitions => this.competitions = competitions);
   }
+
   selectCompetition(competition: Competition) {
     this.currentCompetition = competition;
   }
 
+
+
+  getCompetitions(page: number = 0) {
+    this.page = 0;
+    if (page < this.competitions.totalPages && page >= 0) {
+      this.page = page;
+      if (this.activeStatus == 'all')
+        this.competitionService.getPaginateCompetitions(this.page).subscribe(competitions => this.competitions = competitions);
+      else
+        this.competitionService.getCompetitionsByStatusPaginate(this.activeStatus, this.page)
+          .subscribe(competitions => this.competitions = competitions);
+    }
+  }
 
 
 
